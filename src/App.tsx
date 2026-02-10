d7baf74fb3ce5e7b3101d35747201341e0f56084
Anp-Bounteous <your-email@example.com>
2026-02-10 19:30:38 +0530

    fix: context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Products from "./pages/products";
 import Product from "./pages/product";
 import Cart from "./pages/cart";
 
-
-const AppRoutes = () => {
+const App = () => {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </BrowserRouter>
   );
 };
 
-export default AppRoutes;
+export default App;
 
 
 
